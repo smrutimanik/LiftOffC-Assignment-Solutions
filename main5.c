05f467b4585bffa1150207380c5d01c71346c6e2
Smruti Ranjan Manik <74336812+smrutimanik@users.noreply.github.com>
2021-02-15 09:42:59 +0530

    Week 2 Solutions

---
+++ main5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+	int i, j, n = 3;
+	int count;
+
+	count = n * 2 - 1;
+
+	for(i=1; i<= count; i++)
+	{
+		for(j=1; j<=count; j++)
+		{
+			if(j==i || (j==count - i + 1))
+			{
+				printf("*");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
